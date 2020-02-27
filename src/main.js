6d359e74c2b1ed6cf0bbce4417320d0fc017ad63
jesdevless <jesica.trejof@gmail.com>
2020-02-27 13:15:14 -0600

    botones email y contraseña

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import { example } from './data.js';
 
+
 console.log(example);
+
+function registrar(){
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+}
