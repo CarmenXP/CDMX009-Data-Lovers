31e7c62fb127e737ebb3aabe71ed41af49763011
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-02-27 15:23:38 -0600

    acceder por facebook

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import { example } from './data.js';
 
+// nodos
+let button = document.querySelector('#facebook')
 
-console.log(example);
 
+function login(){
+  var provider = new firebase.auth.FacebookAuthProvider();
+  //function accederFacebook(){
+    //let logIn = document.getElementById('facebook');
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+    var user = result.user;
+    console.log(user)
+  }).catch(function(error) {
+    console.log(error)
+  });
+}
+
+// listeners
+button.addEventListener("click", login)
+
+
+
+
+
+
+//funciones para botones
 function registrar(){
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
