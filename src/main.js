99dec02d91e00a77e76cf2bc640bd7592566b107
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-03-04 13:07:53 -0600

    envio de info formulario

--- src/main.js
+++ src/main.js
@@ -9,16 +9,17 @@ import {yourHuman,
     yourVampire,
   }
    from './data.js'
-console.log(yourHuman, yourHumanoid,yourAlien,
-yourAnimal,
-yourParasite,
-yourZombie,
-yourRobot,
-yourVampire,
-);
+//console.log(yourHuman, yourHumanoid,yourAlien,
+//yourAnimal,
+//yourParasite,
+//yourZombie,
+//yourRobot,
+//yourVampire,
+//);
 
 // nodos
 let button = document.querySelector('#facebook')
+let form= document.querySelector('#formulario')
 
 function login(){
   var provider = new firebase.auth.FacebookAuthProvider();
@@ -33,19 +34,26 @@ function login(){
 
 // listeners
 button.addEventListener("click", login)
+registro.addEventListener("click", registrar)
+form.addEventListener("submit",getForm)
 
 
 //  registro
 function registrar(){
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+  var email = document.querySelector('#email').value;
+  var password = document.querySelector('#password').value;
   firebase.auth().signInWithEmailAndPassword(email, password)
   .catch(function(error) {
   var errorCode = error.code;
   var errorMessage = error.message;
 });
-  console.log(email);
-  console.log(password);
 }
 
-registro.addEventListener("click", registrar)
+//funcion enviar formulario
+function getForm(e){
+  e.preventDefault()
+  let fo=e.target
+
+  console.log(especie.value)
+  console.log(especie.value)
+}
