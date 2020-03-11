9e8ae8afd5bba94ec86fae33db5c1ed94d6bf388
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-03-11 11:22:16 -0600

    filtrado genero y especie codigo

--- src/data.js
+++ src/data.js
@@ -1,35 +1,13 @@
 import data from './data/rickandmorty/rickandmorty.js';
 
-/* Una función que retorna una función (usando bloques y retorno
-  explícito para ilustrar el concepto).
-
-  const fn = () => {
-  return () => {
-    return true;
-  };
-};
-
-La misma función usando retornos implícitos
-const fn = () => () => true; */
-
-export const getSpecie = data.results.filter((character) => character.species);
-export const getGender = data.results.filter((character) => character.gender);
-
-export function getCharsBySpecie(specie, array){
-  let list =  array.filter((character) => character.species===specie);
-  return list
-}
-
+//filter by Gender
 export function getCharsByGender(gender){
   let list =  data.results.filter((character) => character.gender===gender);
   return list
 }
 
-export const yourHuman = data.results.filter((character) => character.species === 'Human');
-export const yourHumanoid = data.results.filter((character) => character.species === 'Humanoid');
-export const yourAlien = data.results.filter((character) => character.species === 'Alien');
-export const yourAnimal = data.results.filter((character) => character.species === 'Animal');
-export const yourParasite = data.results.filter((character) => character.species === 'Parasite');
-export const yourZombie = data.results.filter((character) => character.species === 'Zombie');
-export const yourRobot = data.results.filter((character) => character.species === 'Robot');
-export const yourVampire = data.results.filter((character) => character.species === 'Vampire');
+//filter by specie
+export function getCharsBySpecie(specie, array){
+  let list =  array.filter((character) => character.species===specie);
+  return list
+}
--- src/main.js
+++ src/main.js
@@ -1,27 +1,23 @@
 import data from './data/rickandmorty/rickandmorty.js';
-import {getSpecie,
-    getGender,
-    yourHuman,
-    getCharsBySpecie,
-    getCharsByGender
+import {getCharsBySpecie,
+  getCharsByGender
   }
    from './data.js'
 console.log(getSpecie,getGender);
 
 // nodos
 let button = document.querySelector('#facebook')
-let form = document.querySelector(".persona")
-let profile = [];
-let character = document.querySelector("#enviar")
+let select = document.querySelector('.persona')
 let especieSelect = document.querySelector('#especie')
 let genderSelect = document.querySelector('#genero')
+let getYourCharacter = document.querySelector('#enviar')
 
 // listeners
-especieSelect.addEventListener('change', getForm)
 button.addEventListener("click", login)
 registro.addEventListener("click", registrar)
-form.addEventListener("change",getForm)
-character.addEventListener("click", getCharacter)
+especieSelect.addEventListener('change', getForm)
+select.addEventListener("change", getGenderAndSpecie)
+getYourCharacter.addEventListener("click", getCharacter)
 
 //  login
 function login(){
@@ -46,42 +42,12 @@ function registrar(){
 }
 
 //array género y especie
-function getForm(e){
+function getGenderAndSpecie(e){
   let specie = e.target.value
   let gender = genderSelect.value
   let list = getCharsByGender(gender)
   let list2 = getCharsBySpecie(specie, list)
   console.log(list2)
-  // Dibujar en el dom
-
 }
 
-//obtener personaje filtrado
-function getCharacter (profile, e){
-  e.preventDefault()
-
-  console.log(profile)
-
-};
-
-  //function getCharacter(profile, getSpecie, e) {
-  //  e.preventDefault()
-  //  specieList = [];
-  //  for (var i = 0; i < profile.length; i++) {
-  //    specieList.push(getSpecie(profile[i].value));
-  //  }
-  //  console.log(specieList);
-  //}
-
-
-//funcion lista especies y género
-//for(let i = 0; i < specieList.length ; i++) {
-//  saveSpecie.push(specieList[i].value);
-//};
-//console.log(saveSpecie)
-//
-//for(let i=0; i<genderList.length; i++){
-//  saveGender.push(genderList[i].value);
-//};
-//
-//console.log(genderList.value)
+//dom personaje filtrado
